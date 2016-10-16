6eea612beca4f4c11727890c1c3514e692ddeced
Alexandr Dukhno <a.dukhno1205@gmail.com>
2016-10-16 14:52:46 +0300

    sentinel linear search

--- linearSearch/LinearSearch.cs
+++ linearSearch/LinearSearch.cs
@@ -10,18 +10,13 @@ namespace linearSearch
     {
         public static void Search(string[] strS,string str)
         {
-                int? answer = null;
-                for (int i = 0; i < strS.Length; i++)
-                {
-                    if (str.ToLower() == strS[i].ToLower()) 
-                    {
-                        answer = i;
-                    }
-                }
-                if (answer != null)
-                {
-
-                    Console.WriteLine("Искомый элемент находится на позиции: {0}\nЭлемент: {1}", answer + 1, strS[(int)answer]);
+                int i = 0;
+                string last = strS[strS.Length-1];
+                strS[strS.Length-1] = str;
+                while (strS[i] != str) i++;
+                strS[strS.Length - 1] = last;
+                if (i < strS.Length || strS[strS.Length - 1] == str) {
+                Console.WriteLine("Искомый элемент находится на позиции: {0}\nЭлемент: {1}", i + 1, strS[i]);
                 }
                 else 
                 {
